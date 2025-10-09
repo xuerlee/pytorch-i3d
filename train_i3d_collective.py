71e45257eb7530442e1d7349787b201032a409f4
xuerlee <xuer0324@sina.com>
2025-10-08 21:39:07 -0400

    i3d original dataset

--- train_i3d_collective.py
+++ train_i3d_collective.py
@@ -154,7 +154,6 @@ def run(init_lr=0.1, max_steps=64e3, mode='rgb', root='/ssd/Charades_v1_rgb', tr
                         # print('{} Loc Loss: {:.4f} Cls Loss: {:.4f} Tot Loss: {:.4f}'.format(phase, tot_loc_loss/(10*num_steps_per_update), tot_cls_loss/(10*num_steps_per_update), tot_loss/10))
                         print('{} Tot Loss: {:.4f}'.format(phase, tot_loss/10))
                         writer.add_scalar("Train/Loss", tot_loss / 10, steps)
-                        writer.add_scalar("Train/Error", error, steps)
                         tot_loss  = tot_cls_loss = 0.
                     if steps % 100 == 0:
                         torch.save(i3d.module.state_dict(), save_model + '/'+ str(steps).zfill(6) + '.pt')
